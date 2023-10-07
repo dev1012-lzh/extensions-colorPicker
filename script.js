65b413b05e18a8774402c5da69f596008657ae7a
dev1012-lzh <143276208+dev1012-lzh@users.noreply.github.com>
2023-10-07 17:57:13 +0800

    Update

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $('#text').autosize();
+});
+
+var d = document;
+
+d.addEventListener('DOMContentLoaded', function () {
+
+    var savedContent = localStorage.getItem("notepadcontent");
+    if (savedContent != null) {
+        d.getElementById("text").value = savedContent;
+    }
+
+
+    d.getElementById("text").onkeyup = function () {
+        var data = d.getElementById("text").value; localStorage.setItem("notepadcontent", data);
+    }
+});
+
